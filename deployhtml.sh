e59b9a0c367ed31b9ae47fccf2046eeacb638395
pheolix <f3liix@hotmail.com>
2024-02-25 23:53:58 +0100

    react app launch

---
+++ deployhtml.sh
@@ -0,0 +1,2 @@
+sudo rm /var/www/html/pixelboard/*
+sudo cp -a /home/felix/pixelboard/frontend/build/. /var/www/html/pixelboard/
--- frontend/test.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-
-resp = requests.post('http://127.0.0.1:8000/games',data=json.dumps({"number": 2}))
-
-print(resp.text.encode('utf-8'))
